423cd748af7788ce80589fc4723327d92bbbe3de
srkrishna3384 <srkrishna3384@gmail.com>
2020-09-26 18:10:19 +0530

    Added TestTwo.java

---
+++ src/test/java/com/rameshsoft/git/GitTest/TestTwo.java
@@ -0,0 +1,11 @@
+package com.rameshsoft.git.GitTest;
+
+import org.testng.annotations.Test;
+
+public class TestTwo {
+	@Test
+	public void demoTwo() {
+		System.out.println("Test Two Class");
+	}
+
+}
