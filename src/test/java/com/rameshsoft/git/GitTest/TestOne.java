224dce11186381468efeb559eb8efa5b73d5247c
srkrishna3384 <srkrishna3384@gmail.com>
2020-09-26 18:03:24 +0530

    Added new project

---
+++ src/test/java/com/rameshsoft/git/GitTest/TestOne.java
@@ -0,0 +1,11 @@
+package com.rameshsoft.git.GitTest;
+
+import org.testng.annotations.Test;
+
+public class TestOne {
+	@Test
+	public static void demoOne() {
+		System.out.println("Test One Class");
+	}
+
+}
